4f8b315c41f22d7c04dd651fbd999bf4ea972f37
joshluisaac <nwankwo.joshua@gmail.com>
2016-07-03 03:06:38 +0800

    Inital commit

---
+++ MergeSorterTest.java
@@ -0,0 +1,24 @@
+package taojava.labs.sorting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Some simple tests of our Merge sort routine.
+ */
+public class MergeSorterTest
+{
+  @Test
+  public void test1()
+  {
+    TestUtils.test1(new MergeSorter<Integer>());
+  } // test1
+
+  @Test
+  public void test2()
+  {
+    TestUtils.test2(new MergeSorter<Integer>());
+  } // test2
+
+} // MergeSorterTest
